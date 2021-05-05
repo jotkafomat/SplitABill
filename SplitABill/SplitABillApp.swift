8cd592d647d897bb31219b984237c67f61800bd5
jotkafomat <k-j@xl.wp.pl>
2021-05-05 10:43:16 +0100

    Initial Commit

---
+++ SplitABill/SplitABillApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitABillApp.swift
+//  SplitABill
+//
+//  Created by Krzysztof Jankowski on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SplitABillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
